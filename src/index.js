1dae6838ccd241ec476d285553d8821e782abe7c
kim0612 <roki03@naver.com>
2021-03-29 15:27:58 +0900

    #1.0_VanillaCode
    simple calc _by VanillaJS

--- src/index.js
+++ src/index.js
@@ -14,3 +14,30 @@
 // // to log results (for example: reportWebVitals(console.log))
 // // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 // reportWebVitals();
+
+
+
+const plus = document.getElementById("plus");
+const minus = document.getElementById("minus");
+const num = document.querySelector("span");
+let count = 12;
+
+
+const updateNum = () => {
+  num.innerHTML = count;
+}
+const handlePlus = () => {
+  console.log("plus");
+  count = count+1;
+  updateNum();
+}
+const handleMinus = () => {
+  console.log("Minus");
+  count = count-1;
+  updateNum();
+}
+
+
+plus.addEventListener("click", handlePlus);
+minus.addEventListener("click", handleMinus);
+num.innerHTML = count;
