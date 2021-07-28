386cf9b2e878728692c2109a64fb59e3c0a4e0e6
kim0612 <roki03@naver.com>
2021-07-28 22:08:55 +0900

    --------
    작업환경 변경
    -> mac book pro

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-// function App() {
-//   return (
-//     <div className="App">
-//       hello react-redux
-//     </div>
-//   );
-// }
+// React 코드 삭제!!
+/*
+function App() {
+  return (
+    <div className="App">
+      hello react-redux
+    </div>
+  );
+}
 
-// export default App;
+export default App;
+*/
